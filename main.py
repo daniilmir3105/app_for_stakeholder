778c7508e8995eb345f816f3e09b0ce2925e59a1
unknown <miroshnichienko.daniil@gmail.com>
2021-06-01 22:28:27 +0200

    trying to make in main.py logo

--- main.py
+++ main.py
@@ -23,6 +23,18 @@ analys_param_8 = base_class_analys.communicativity()
 # making object to become a result 
 final_result = become_result.result()
 
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+Logo = resource_path("Logo.png")
+
 class Stakeholder(QtWidgets.QMainWindow, Ui_Dialog):
     '''
     This class will have all fields and methods for stakeholder analys
